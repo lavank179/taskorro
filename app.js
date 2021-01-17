fd41897ccd17d716afe0081fc64b92abcf8dc462
lavank179 <chlavankumar179@gmail.com>
2021-01-17 18:16:38 +0530

    remove and clear events completed

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ loadEventListeners();
 function loadEventListeners() {
     // Add task event
     form.addEventListener('submit', addTask);
+    // Remove task event
+    taskList.addEventListener('click', removeTask);
+    // Clear task event
+    cleasBtn.addEventListener('click', clearTasks);
 }
 
 // Add Task
@@ -41,4 +45,27 @@ function addTask(e){
     e.preventDefault();
 
 
+}
+
+// Remove task event
+function removeTask(e){
+
+    if(e.target.parentElement.classList.contains('delete-item')) {
+        if(confirm('Are you sure?')){
+            e.target.parentElement.parentElement.remove();
+        }
+        
+    }
+
+    e.preventDefault();
+}
+
+// Clear Tasks
+function clearTasks(){
+    // taskList.innerHTML = '';
+
+    // Faster
+    while(taskList.firstChild){
+        taskList.removeChild(taskList.firstChild);
+    }
 }
